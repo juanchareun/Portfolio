e7989bd709916f266898bb37e4f10577624fd77f
Juan Chareun <juanignaciochareun@gmail.com>
2025-12-05 12:18:46 -0500

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
   const contactInfo = [
     {
       icon: Mail,
-      label: "mail",
+      label: "Email",
       value: "juanignaciochareun@gmail.com",
       href: "mailto:juanignaciochareun@gmail.com",
     },
